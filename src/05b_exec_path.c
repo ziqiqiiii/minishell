4a858ba4997b78490adec1c1e31618e094f49d18
Thong Zi Qi <ziqi.thong@gmail.com>
2026-06-09 23:23:43 +0800

    refactor - improve get_exe_path and join_path functions for better path resolution

--- src/05b_exec_path.c
+++ src/05b_exec_path.c
@@ -1,83 +1,39 @@
 #include "minishell.h"
 
-/**
- * @brief Appends a "/" to the end of each string in a path array.
- *
- * This function modifies the array in place. It prints an error message and
- * returns if an error occurs during memory allocation.
- *
- * @param path An array of strings representing paths.
- */
-static void	path_helper(char **path)
-{
-	int		i;
-	char	*temp;
-
-	i = -1;
-	while (path[++i])
-	{
-		temp = ft_strjoin(path[i], "/");
-		if (temp != NULL)
-		{
-			free(path[i]);
-			path[i] = temp;
-		}
-		else
-		{
-			printf("Error: Path Helper Failed: %s\n", strerror(errno));
-			return ;
-		}
-	}
-}
+char		*get_exe_path(char *argv, t_list **env_list);
+char		*join_path(t_list **env_list, char *cmd);
+char		**find_path(t_list **env_list);
+static void	path_helper(char **path);
+static char	*join_path_helper(char *cmd, char **tmp, int i);
 
 /**
- * @brief Finds the system PATH.
+ * @brief Determines the full path of an executable given its name.
  *
- * This function splits the PATH environment variable into an array of
- * strings, adds a "/" to the end of each path, and returns the array.
+ * Splits the provided command into words, checks if any of the path
+ * strings stored in PATH match the command name, then joins the path
+ * with the command if a match is found and the file exists.
  *
- * @return An array of strings representing the system PATH, or NULL if an
- * error occurs.
+ * @param argv The command to find the path for.
+ * @return The full path of the executable if found, NULL otherwise.
  */
-char	**find_path(t_list **env_list)
+char	*get_exe_path(char *argv, t_list **env_list)
 {
-	char	*tmp;
-	char	**path;
-
-	tmp = existed_env("PATH", env_list);
-	if (tmp != NULL)
-	{
-		path = ft_split(tmp, ':');
-		path_helper(path);
-		return (path);
-	}
-	return (NULL);
-}
-
-/**
- * @brief Helper function to join a command with a path and check file access.
- *
- * This function constructs a full path by joining the command with a path from
- * the PATH array. If the file exists, it returns the path, otherwise NULL.
- *
- * @param cmd Command name.
- * @param tmp Array of path strings.
- * @param i Index of the current path in the array.
- * @return Full path of the executable if found, NULL otherwise.
- */
-static char	*join_path_helper(char *cmd, char **tmp, int i)
-{
+	char	*cmd;
 	char	*join;
 
-	join = ft_strjoin(tmp[i], cmd);
-	if (access(join, F_OK) == 0)
+	cmd = ft_strdup(argv);
+	if (ft_strrchr(cmd, '/') != NULL)
 	{
-		free (cmd);
-		free_2d(tmp);
-		return (join);
+		if (access(cmd, X_OK) == 0)
+			return (cmd);
+		else
+		{
+			free(cmd);
+			return (NULL);
+		}
 	}
-	free(join);
-	return (NULL);
+	join = join_path(env_list, cmd);
+	return (join);
 }
 
 /**
@@ -115,33 +71,85 @@ char	*join_path(t_list **env_list, char *cmd)
 }
 
 /**
- * @brief Determines the full path of an executable given its name.
+ * @brief Finds the system PATH.
  *
- * Splits the provided command into words, checks if any of the path
- * strings stored in PATH match the command name, then joins the path
- * with the command if a match is found and the file exists.
+ * This function splits the PATH environment variable into an array of
+ * strings, adds a "/" to the end of each path, and returns the array.
  *
- * @param argv The command to find the path for.
- * @return The full path of the executable if found, NULL otherwise.
+ * @return An array of strings representing the system PATH, or NULL if an
+ * error occurs.
  */
-char	*get_exe_path(char *argv, t_list **env_list)
+char	**find_path(t_list **env_list)
 {
-	char	*cmd;
-	char	*join;
+	char	*tmp;
+	char	**path;
 
-	cmd = ft_strdup(argv);
-	if (ft_strrchr(cmd, '/') != NULL)
+	tmp = existed_env("PATH", env_list);
+	if (tmp != NULL)
 	{
-		if (access(cmd, X_OK) == 0)
-			return (cmd);
-		else
-		{
-			free(cmd);
-			return (NULL);
-		}
+		path = ft_split(tmp, ':');
+		path_helper(path);
+		return (path);
 	}
-	join = join_path(env_list, cmd);
-	if (join != NULL)
-		return (join);
 	return (NULL);
 }
+
+
+/**
+ * @brief Appends a "/" to the end of each string in a path array.
+ *
+ * This function modifies the array in place. It prints an error message and
+ * returns if an error occurs during memory allocation.
+ *
+ * @param path An array of strings representing paths.
+ */
+static void	path_helper(char **path)
+{
+	int		i;
+	char	*temp;
+
+	i = -1;
+	while (path[++i])
+	{
+		temp = ft_strjoin(path[i], "/");
+		if (temp != NULL)
+		{
+			free(path[i]);
+			path[i] = temp;
+		}
+		else
+		{
+			printf("Error: Path Helper Failed: %s\n", strerror(errno));
+			return ;
+		}
+	}
+}
+
+
+/**
+ * @brief Helper function to join a command with a path and check file access.
+ *
+ * This function constructs a full path by joining the command with a path from
+ * the PATH array. If the file exists, it returns the path, otherwise NULL.
+ *
+ * @param cmd Command name.
+ * @param tmp Array of path strings.
+ * @param i Index of the current path in the array.
+ * @return Full path of the executable if found, NULL otherwise.
+ */
+static char	*join_path_helper(char *cmd, char **tmp, int i)
+{
+	char	*join;
+
+	join = ft_strjoin(tmp[i], cmd);
+	if (access(join, F_OK) == 0)
+	{
+		free (cmd);
+		free_2d(tmp);
+		return (join);
+	}
+	free(join);
+	return (NULL);
+}
+
+
